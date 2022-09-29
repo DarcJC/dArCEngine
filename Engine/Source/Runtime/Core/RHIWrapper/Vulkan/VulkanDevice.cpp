95b59f1ebecdf136d74a57a5a219c9687292a1b6
darczeng <me@darc.pro>
2022-09-29 20:41:36 +0800

    🎉feat: upload device feature

--- Engine/Source/Runtime/Core/RHIWrapper/Vulkan/VulkanDevice.cpp
+++ Engine/Source/Runtime/Core/RHIWrapper/Vulkan/VulkanDevice.cpp
@@ -8,3 +8,25 @@ VulkanDevice::VulkanDevice(VulkanDynamicRHI *inRHI, vk::raii::PhysicalDevice inH
     , physicalHandle(std::move(inHandle)) {
     ensure(nullptr != inRHI, "[VulkanDevice::VulkanDevice] RHI pointer should not be null");
 }
+
+void VulkanDevice::init() {
+    simple_queue_indices_ = DeviceQueueIndices::NewQueueIndices(physicalHandle);
+
+    float graphicPriority[] { 1.0f };
+    vk::DeviceQueueCreateInfo graphicQueueCI{
+        {},
+        simple_queue_indices_->graphic.value(),
+        1,
+        graphicPriority
+    };
+
+    std::vector<vk::DeviceQueueCreateInfo> queues { graphicQueueCI };
+
+    vk::DeviceCreateInfo {
+        {},
+        queues,
+        {},
+        {},
+        {}
+    };
+}
--- Engine/Source/Runtime/Core/RHIWrapper/Vulkan/VulkanDevice.h
+++ Engine/Source/Runtime/Core/RHIWrapper/Vulkan/VulkanDevice.h
@@ -1,5 +1,8 @@
 #pragma once
+
+#include <optional>
 #include "vulkan/vulkan_raii.hpp"
+#include "VulkanQueue.h"
 
 class VulkanDynamicRHI;
 
@@ -9,8 +12,12 @@ public:
     VulkanDevice() = delete;
     VulkanDevice(VulkanDynamicRHI* rhi, vk::raii::PhysicalDevice physicalDevice);
 
+    void init();
+
 private:
     VulkanDynamicRHI* rhi;
     vk::raii::PhysicalDevice physicalHandle;
+    std::optional<vk::raii::Device> device_;
+    std::optional<DeviceQueueIndices> simple_queue_indices_;
 
 };
